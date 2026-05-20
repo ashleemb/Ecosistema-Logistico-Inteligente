19e9ea1900405c4eac4c0b2a1e9fd143a6a2b279
Jimena Pintos <jimena.pintos@fiuni.edu.py>
2026-05-20 20:45:13 -0300

    Actualización de VehiculoRepositorio

--- src/repositorio/VehiculoRepositorio.java
+++ src/repositorio/VehiculoRepositorio.java
@@ -22,4 +22,33 @@ public class VehiculoRepositorio {
         return listaVehiculos;
     }
 
+    public Vehiculo buscarPorID(String id){
+        for(Vehiculo vehiculo : listaVehiculos){
+            if(vehiculo.getId().equals(id)){
+                return vehiculo;
+            }
+        }
+        return null;
+    }
+
+    public boolean modificarVehiculo(String id, Vehiculo vehiculoModificado){
+        Vehiculo encontrado = buscarPorID(id);
+        int indice = listaVehiculos.indexOf(encontrado);
+
+        if(encontrado != null){
+            listaVehiculos.set(indice, vehiculoModificado);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean eliminarVehiculo(String id){
+        Vehiculo v = buscarPorID(id);
+
+        if(v != null){
+            listaVehiculos.remove(v);
+            return true;
+        }
+        return false;
+    }
 }
